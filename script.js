866ad886416715ba5bbc7c769f5330e82b356959
Alice Gonzalez <alicegcodes@gmail.com>
2024-01-29 22:26:20 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const input = document.querySelector('#fruit'); //Search bar
-const suggestions = document.querySelector('.suggestions ul'); //oOntainer for all suggestions
+const input = document.querySelector('#fruit'); //Selects HTML search bar
+const suggestions = document.querySelector('.suggestions ul'); //Selects HTML container for all suggestions
 
-const fruit = ['Apple 🍎', 'Apricot', 'Avocado 🥑', 'Banana 🍌', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry 🫐', 'Boysenberry', 'Currant', 'Cherry 🍒', 'Coconut 🥥', 'Cranberry', 'Cucumber 🥒', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape 🍇', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit 🥝', 'Kumquat', 'Lemon 🍋', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon 🍍', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine 🍊', 'Papaya', 'Passionfruit', 'Peach 🍑', 'Pear 🍐', 'Persimmon', 'Plantain', 'Plum', 'Pineapple 🍍', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry 🍓', 'Tamarillo', 'Tamarind', 'Yuzu']; 
+const fruit = ['Apple 🍎', 'Apricot', 'Avocado 🥑', 'Banana 🍌', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry 🫐', 'Boysenberry', 'Currant', 'Cherry 🍒', 'Coconut 🥥', 'Cranberry', 'Cucumber 🥒', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape 🍇', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit 🥝', 'Kumquat', 'Lemon 🍋', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon 🍍', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine 🍊', 'Papaya', 'Passionfruit', 'Peach 🍑', 'Pear 🍐', 'Persimmon', 'Plantain', 'Plum', 'Pineapple 🍍', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry 🍓', 'Tamarillo', 'Tamarind', 'Yuzu']; //List of fruits
 
 //Function to filter list of fruits array and return result
 function search(str) {
-	return fruit.filter(fruitName => fruitName.toLowerCase().includes(str.toLowerCase()));
+	return fruit.filter(fruitName => fruitName.toLowerCase().includes(str.toLowerCase())); //Filter through array of fruit and compare if lower case version of str any string of fruit in the array.Returns a new array with only fruit names in search lower case.
 }
 
 //Function  that executes whe user types
 function searchHandler(e) {
-	const inputValue = e.target.value;
-	if (/[^a-zA-Z]/.test(inputValue)) { //If input is not a letter from a-z (uppercase or lowercase)
-		alert('🛑Please enter letters only.😐'); //Print this alert
+	const inputValue = e.target.value; //Variable that retrieves the value entered by the user from the event object
+	if (/[^a-zA-Z]/.test(inputValue)) { //If inputValue is not a letter from a-z (uppercase or lowercase)
+		alert('🛑Please enter letters only.😐'); //Print this alert and prevent rest of function from executing
 		return;
 	}
-	const results = search(inputValue);
-	showSuggestions(results, inputValue)
+	const results = search(inputValue); //Variable that gives results by calling search function and passing InputValue as an argument
+	showSuggestions(results, inputValue) //Calling function showSuggestions with 2 arguments. Show fruit suggestions based on input.
 }
 
 //Function that shows fruit suggestion that include letters typed by the user
 function showSuggestions(results, inputVal) { 
 
-	suggestions.innerHTML = ''; //Set HTML content to empty string
+	suggestions.innerHTML = ''; //Set HTML suggestions content to empty string. Clears existing content.
 
-	if(inputVal.length > 0) { //If the inputValue is more than 0
-		results.forEach(results => { //Iterate through the results
+	if(inputVal.length > 0) { //Conditional Statement: If the inputValue is more than 0 characters
+		results.forEach(results => { //Iterate through the results array
 			const li = document.createElement('li'); //Create a new list element
-			const boldResults= results.replace(new RegExp(`(${inputVal})`, 'ig'), '<span class="has-suggestions">$1</span>'); //Create bold style and save to a variable
-			li.innerHTML = boldResults; //Apply boldResults style to each list item
-			suggestions.appendChild(li); //Add item to the list
+			const boldResults= results.replace(new RegExp(`(${inputVal})`, 'ig'), '<span class="has-suggestions">$1</span>'); //Create bold style and store to a variable
+			li.innerHTML = boldResults; //Apply boldResults style to each list item that matches part of the result
+			suggestions.appendChild(li); //Add fruit suggestions to the list
 		});
 	}
 }
 
-//Function that populates the search bar with the fruit  user clicks on.
+//Function that populates the search bar with the fruit user clicks on.
 function useSuggestion(e) {
-	input.value = e.target.innerText; //If the input vale is equal to the target text
-	suggestions.innerHTML = "";
+	input.value = e.target.innerText; //If the input value is equal to the target text
+	suggestions.innerHTML = ""; //Set HTML suggestions content to empty string. Clears existing content.
 }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+input.addEventListener('keyup', searchHandler); //Execute funtion searchHandler when key is released typing input in search box
+suggestions.addEventListener('click', useSuggestion); //Execute function useSuggestions when suggestion element is clicked
